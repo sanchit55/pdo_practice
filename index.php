f57fd6365f5fa77474eed947285150ab1f3709ba
Sanchit Gupta <sg948@afsconnect1.njit.edu>
2017-11-05 10:01:28 -0500

    second

--- index.php
+++ index.php
@@ -8,11 +8,25 @@ $password = 'HfwrZHvX';
 try {
 	$connect = new PDO ($name, $dbname, $username, $password);
 	echo "<p>Connection made successfully</p>". '<br>';
+	$set= $connect->prepare("SELECT * FROM accounts \n" . "where id<6");
+	$set->execute();
+	$records=$set->rowCount();
+	echo '<br>';
+	echo "Number of items with less than id of 6 are: $records". '<br>';
+	$set->execute;
+	$result= $self->setFetchMode(PDO::FETCH_ASSOC);
+
+	foreach(new tablerows(new recursivearrayiterator($self->fetchall())) as $k=>$v)
+	{
+		echo $v;
+	}
 }
 
 catch (PDOException $a) {
 	$error_message = $a -> getMessage();
 	echo "<p> Error Found while connecting to database: $error_message</p>" . '<br>';
 }
+
+ 
 
 ?>
